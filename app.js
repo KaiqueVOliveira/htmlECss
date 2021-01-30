3f5343f7351ca3dd8fff7760cd45bb442139e2c2
KaiqueVOliveira <kaiquevieirao@gmail.com>
2021-01-30 13:23:28 -0300

    estudo de flexbox

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-/*const moment = require("moment");
-
-const fs = require('fs');
-//let momento = require('moment');
-let  dados = fs.readFileSync(__dirname + '/dados.txt', 'utf8');
-
-const superHeroes = require('./SuperHeroes');
-
-let data = moment().format('DD do MMM YY');
-
-console.log(superHeroes);*/
 
 
+    /*const moment = require("moment");
 
-let cachorro = require('./superHeroes')
+    const fs = require('fs');
+    //let momento = require('moment');
+    let  dados = fs.readFileSync(__dirname + '/dados.txt', 'utf8');
 
-let{nome, idade} = cachorro
+    const superHeroes = require('./SuperHeroes');
 
-console.log('O cachorro se chama ' + nome + ' e tem ' + idade + ' anos')
+    let data = moment().format('DD do MMM YY');
+
+    console.log(superHeroes);*/
+
+
+
+    let cachorro = require('./superHeroes')
+
+    let{nome, idade} = cachorro
+
+    console.log('O cachorro se chama ' + nome + ' e tem ' + idade + ' anos')
